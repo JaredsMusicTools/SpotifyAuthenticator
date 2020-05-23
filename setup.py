f62c2a0965af71ffa9f050696564f9e5d76bc709
JaredDyreson <jared.dyreson@gmail.com>
2020-05-22 20:01:56 -0700

    now has a setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import os
+import sys
+
+PKG_NAME = "SpotifyAuthenticator"
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+if sys.version_info[0] < 3:
+    with open(os.path.join(_here, 'README.md')) as f:
+        long_description = f.read()
+else:
+    with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+version = {}
+with open(os.path.join(_here, PKG_NAME, 'version.py')) as f:
+    exec(f.read(), version)
+
+setup(
+    name = PKG_NAME,
+    version=version['__version__'],
+    description=('Flask application for authenticating Spotify users'),
+    long_description=long_description,
+    author='Jared Dyreson',
+    author_email='jareddyreson@csu.fullerton.edu',
+    url='https://github.com/JaredDyreson/SpotifyAuthenticator',
+    license='GNU GPL-3.0',
+    packages=[PKG_NAME],
+    install_requires = [
+      'Flask-OAuthlib==0.9.5',
+      'Flask==1.0.2'
+    ],
+    include_package_data=True,
+    classifiers=['Programming Language :: Python :: 3.8']
+)
