9ca4739dff61d29cc0d9b1b9cb9e097110711b58
JaredDyreson <jared.dyreson@gmail.com>
2020-05-22 20:25:36 -0700

    added try catch around the key retrieval in __init__.py

--- SpotifyAuthenticator/__init__.py
+++ SpotifyAuthenticator/__init__.py
@@ -4,8 +4,12 @@ import os
 PKG_NAME = "SpotifyAuthenticator"
 
 application = Flask(PKG_NAME)
-application.config['SECRET_KEY'] = os.environ.get('SPOTIFY_SECRET_KEY')
-application.config['consumer_key'] = os.environ.get('SPOTIFY_AUTHENTICATOR_CLIENT_ID')
-application.config['consumer_secret'] = os.environ.get('SPOTIFY_AUTHENTICATOR_CLIENT_SECRET')
+
+try:
+  application.config['SECRET_KEY'] = os.environ.get('SPOTIFY_SECRET_KEY')
+  application.config['consumer_key'] = os.environ.get('SPOTIFY_AUTHENTICATOR_CLIENT_ID')
+  application.config['consumer_secret'] = os.environ.get('SPOTIFY_AUTHENTICATOR_CLIENT_SECRET')
+except KeyError as invalid_key:
+    raise KeyError("[-] You need to set {} in your shell's configuration file".format(invalid_key))
 
 from SpotifyAuthenticator import routes
